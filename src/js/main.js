0fadd23fedecefd6bc87a32aef49037b5d6dd461
Nicolas Ledesma <nicolas.ledesma88@gmail.com>
2020-03-12 05:51:42 -0300

    Add NPM dependencies

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import '../scss/main.scss';
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+// Start stimulus
+const application = Application.start();
+const context = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
+
